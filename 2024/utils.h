121e1e93460f3716f22f1bad6306ad94a759eebe
Nicolo <nicoloridulfo@gmail.com>
2024-12-01 18:30:13 +0100

    add benchmark macro

--- 2024/utils.h
+++ 2024/utils.h
@@ -1,2 +1,20 @@
 #pragma once
-char* read_stdin();
+#include <time.h>
+
+char *read_stdin();
+
+#define BENCHMARK_RUNS 10
+#define BENCHMARK(code_block)                                                  \
+        do {                                                                   \
+                double total_time = 0.0;                                       \
+                for (int i = 0; i < BENCHMARK_RUNS; i++) {                     \
+                        clock_t start = clock();                               \
+                        code_block;                                            \
+                        clock_t end = clock();                                 \
+                        double time_taken =                                    \
+                            ((double)(end - start)) / CLOCKS_PER_SEC;          \
+                        total_time += time_taken;                              \
+                }                                                              \
+                printf("Average execution time: %f seconds\n",                 \
+                       total_time / BENCHMARK_RUNS);                           \
+        } while (0)
